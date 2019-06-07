df024d8c81a339c2c5a5b4f6c1bfcdd4c021f116
RalphOb <156w0191@itszongolica.edu.mx>
2019-06-07 14:04:58 -0500

    Primer Commit

---
+++ src/app/data.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable()
+export class DataService {
+  providedIn: 'root'
+  private goals = new BehaviorSubject<any>([]);
+  goal = this.goals.asObservable();
+
+  constructor() { }
+
+  changeGoal(goal){
+    this.goals.next(goal);
+  }
+}
